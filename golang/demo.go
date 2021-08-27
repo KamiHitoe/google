7718c191e79aaf681187e27d9805cddd0cf4f38d
KamiHitoe <sousaku_0912@yahoo.co.jp>
2021-08-27 22:58:55 +0900

    close #1

--- golang/demo.go
+++ golang/demo.go
@@ -1,29 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi %s!", r.URL.Path[1:])
+}
 
 func main() {
-	for i := 0; i < 10; i++ {
-		if i == 3 {
-			continue
-		}
-		fmt.Println(i)
-	}
-
-	// while
-	j := 0
-	for j < 5 {
-		fmt.Println(j)
-		j++
-	}
-
-	// 無限ループ
-	k := 0
-	for {
-		fmt.Println(k)
-		k++
-		if k == 7 {
-			break
-		}
-	}
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
